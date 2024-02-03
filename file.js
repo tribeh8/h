11ec0759187691f1d5773c1f4b995a981d7ed627
tribeh8 <158760065+tribeh8@users.noreply.github.com>
2024-02-03 12:27:19 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,1 @@
-fetch('https://tribeh8.github.io/index.php', {method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({data:'ABC'})});
+fetch('https://eo7ue0lrzs0vw0h.m.pipedream.net', {method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({data:'ABC'})});
