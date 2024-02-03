4aeb5640da99a1b38c9b04c0aed32d312fdc9558
tribeh8 <158760065+tribeh8@users.noreply.github.com>
2024-02-03 11:32:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-header('Access-Control-Allow-Methods:  POST, PUT, GET');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
 file_put_contents('data.txt', print_r(json_decode(file_get_contents("php://input"), true), true));
 ?>
