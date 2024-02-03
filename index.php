4dc0433fd362b940eebdd8cbd5cd866de7e95477
tribeh8 <158760065+tribeh8@users.noreply.github.com>
2024-02-03 11:30:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+header('Access-Control-Allow-Methods:  POST, PUT, GET');
 file_put_contents('data.txt', print_r(json_decode(file_get_contents("php://input"), true), true));
 ?>
