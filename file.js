22ba4fe71592bd369050ab7b7e8eae020e4ae3bb
tribeh8 <158760065+tribeh8@users.noreply.github.com>
2024-02-03 11:18:49 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,1 @@
-fetch('index.php', {method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({data:'ABC'})});
+fetch('https://tribeh8.github.io/index.php', {method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({data:'ABC'})});
